484946804d6365e74d1533d4a85f92263c98c90d
lizardoboi <92044083+lizardoboi@users.noreply.github.com>
2025-07-14 19:26:17 +0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
     process.env.DB_PASS,
     {
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT || 5432,
+        port: process.env.DB_PORT,
         dialect: 'postgres',
         logging: false,
     }
